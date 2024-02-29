80d7b0817d8106c6e929cf6045dd6e8a0514c65e
abijij <105397465+abijij@users.noreply.github.com>
2024-02-28 21:10:05 -0600

    Implementacion de crud en productos y items

--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -7,12 +7,20 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.springboot.app.productos.models.entity.Producto;
 import com.formacionbdi.springboot.app.productos.models.service.IProductoService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 @RestController
 public class ProductoController {
@@ -37,14 +45,14 @@ public class ProductoController {
 	
 	@GetMapping("/ver/{id}")
 	public Producto detalle(@PathVariable Long id) throws InterruptedException {
+		// Forma de generar interferencia para pruebas
+		// if(id.equals(10L)) {
+		// 	throw new IllegalStateException("Producto no encontrado!");
+		// }
 		
-		if(id.equals(10L)) {
-			throw new IllegalStateException("Producto no encontrado!");
-		}
-		
-		if(id.equals(7L)) {
-			TimeUnit.SECONDS.sleep(5L);
-		}
+		// if(id.equals(7L)) {
+		// 	TimeUnit.SECONDS.sleep(5L);
+		// }
 		
 		Producto producto = productoService.findById(id);
 		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
@@ -52,5 +60,30 @@ public class ProductoController {
 				
 		return producto;
 	}
+
+	@PostMapping("/crear")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto crear(@RequestBody Producto producto) {
+		
+		return  productoService.save(producto);
+	}
+
+	@PutMapping("/editar/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto editar(@RequestBody Producto producto, @PathVariable Long id) {
+		Producto productoDb = productoService.findById(id);
+
+		productoDb.setNombre(producto.getNombre());
+		productoDb.setPrecio(producto.getPrecio());
+		
+		return productoService.save(productoDb);
+	}
+
+	@DeleteMapping("/eliminar/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void eliminar(@PathVariable Long id){
+		productoService.deleteById(id);
+	}
+	
 
 }
